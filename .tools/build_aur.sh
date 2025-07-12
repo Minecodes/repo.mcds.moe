5b4ce245c08fb63a542bebafb9ed8fd0d9c457b2
Thies Ohme <contact@mcds.moe>
2025-07-12 21:02:01 +0200

    Debugging build script

--- .tools/build_aur.sh
+++ .tools/build_aur.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
+echo "Updating system..."
 pacman -Syu --noconfirm
+echo "Installing dependencies..."
 pacman -S --noconfirm base-devel git go python3 python-pip gpg sudo
 useradd -m -G wheel -s /bin/bash builder
 echo "builder ALL=(ALL) NOPASSWD: ALL" > /etc/sudo
 sudo -u builder gpg --import signkey.asc
 pacman-key --add signkey.asc
 cp .config/makepkg.conf /etc/makepkg.conf
+echo "Building pikaur..."
 cd build/pikaur || exit 1
 sudo -u builder makepkg -sr --sign --noconfirm || exit 1
 cd ../../public/arch/x86_64 || exit 1
