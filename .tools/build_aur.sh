fe79a713f74619c3c8819a106b0162b54bc01609
Thies Ohme <contact@mcds.moe>
2025-07-12 21:08:24 +0200

    6cfe4c87c2e6cf978c7007ad4aabdc0ee93d2726

--- .tools/build_aur.sh
+++ .tools/build_aur.sh
@@ -3,13 +3,20 @@ echo "Updating system..."
 pacman -Syu --noconfirm
 echo "Installing dependencies..."
 pacman -S --noconfirm base-devel git go python3 python-pip gpgme sudo || exit 1
+
+
 useradd -m -G wheel -s /bin/bash builder
 echo "builder ALL=(ALL) NOPASSWD: ALL" > /etc/sudo
 chown -R builder:builder /home/builder
 chown builder:builder signkey.asc
+chown -R builder:builder /build/build
+chown -R builder:builder /build/build/*
+
 sudo -u builder gpg --import signkey.asc
 pacman-key --add signkey.asc
 cp .config/makepkg.conf /etc/makepkg.conf
+
+
 echo "Building pikaur..."
 cd build/pikaur || exit 1
 sudo -u builder makepkg -sr --sign --noconfirm || exit 1
