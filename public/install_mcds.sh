f318e44024f4e91396e2832f297e792638c2b917
Thies Ohme <contact@mcds.moe>
2025-07-16 20:44:18 +0200

    Corrected install script

--- public/install_mcds.sh
+++ public/install_mcds.sh
@@ -8,7 +8,7 @@ pacman-key --init
 pacman-key --recv-keys 6E2ED1AF080C73ABDD7031C5A81F296D72A1530A --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 6E2ED1AF080C73ABDD7031C5A81F296D72A1530A
 
-echo -e '[mcds]\nServer = https://repo.mcds.moe/arch/$(uname -m)' | tee -a /etc/pacman.conf
+echo -e "[mcds]\nServer = https://repo.mcds.moe/arch/$(uname -m)" | tee -a /etc/pacman.conf
 pacman -Sy
 pacman -S --noconfirm mcds-keyring
 pacman-key --populate mcds
