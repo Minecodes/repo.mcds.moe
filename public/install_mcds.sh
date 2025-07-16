d2802c7a13e458ad86e7ea43bad970bf08796a4f
Thies Ohme <contact@mcds.moe>
2025-07-16 20:12:11 +0200

    Fixed install script

--- public/install_mcds.sh
+++ public/install_mcds.sh
@@ -10,5 +10,5 @@ pacman-key --lsign-key 6E2ED1AF080C73ABDD7031C5A81F296D72A1530A
 
 echo -e '[mcds]\nServer = https://repo.mcds.moe/arch/$(uname -m)' | tee -a /etc/pacman.conf
 pacman -Sy
-pacman -S --no-confirm mcds-keyring
+pacman -S --noconfirm mcds-keyring
 pacman-key --populate mcds
